9dcd3e201e6814eba09126fa2670445c56602ce2
Anastacia <anaanestesia@gmail.com>
2023-10-20 12:32:05 +0100

    mariadb ready

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \'${DATABASE}\';"
+mysql -e "CREATE USER IF NOT EXISTS \'${DB_USER}\'@'localhost' IDENTIFIED BY ${DB_PASSWORD};"
+mysql -e "GRANT ALL PRIVILEGES ON \'${DATABASE}\'.* TO \'${DB_USER}\'@'%' IDENTIFIED BY ${DB_PASSWORD};"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+exec mysqld_safe
