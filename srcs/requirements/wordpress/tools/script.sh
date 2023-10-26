76ab0a67fe92300736f91a63a013bfe1d932b671
ansilva- <ansilva-@debian.ansilva->
2023-10-26 17:59:25 +0100

    dunno

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash/
 
-mkdir -p /run/php/
+mkdir -p /run/php
 
 #Configure PHP-fpm to listen on port 9000
-sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
+chown -R www-data:www-data /var/www/wordpress && chmod 777 /var/www/wordpress
 chmod 755 /var/www/wordpress/wp-config.php
 
 # Database settings on wordpress config
